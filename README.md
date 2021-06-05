eb99888fae056075fa0c290dd8dcc12aca6cdf8c
Gabriel Miranda <vytorgabriel123v@gmail.com>
2021-06-05 00:21:09 -0300

    Finish README and API

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # moeda-api
 
+
+<p align="center">
+ <img src="https://user-images.githubusercontent.com/57048555/120878535-852b3d80-c593-11eb-9d26-73760be1bc47.jpgf" alt="Print" />
+</p>
+
+
+<p align="center">
+ <img src="https://img.shields.io/static/v1?label=PRs&message=welcome&color=49AA26&labelColor=000000" alt="PRs welcome!" />
+</p>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;
+</p>
+
+## 🚀 Tecnologias
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTMl
+- VUE.JS
+- CSS
+- NODE.JS
+- AXiOS
+
+## 🚀 Projeto
+
+- Projeto feito com o foco de aprendizado com o framework Vue.js aplicando uma API com valores de moedas
+
+
 ## Project setup
 ```
 yarn install
@@ -22,3 +51,5 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+Feito com ♥ by Gabriel Miranda
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
+  
 <template>
-  <div class="container grid-lg">
-    <input v-model="quotes.test">
-    <h1>{{quotes}}</h1>
+  <div class="container grid-lg my-2 py-2">
+    <div class="card mb-2" v-if="listenQuotes.length > 0">
+      <div class="card-header">
+        <div class="h4">Acompanhando</div>
+      </div>
+      <div class="card-body">
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-header">
+        <div class="h4">Todas as moedas</div>
+      </div>
+      <div class="card-body">
+        <ListQuotes
+          :quotes="quotes"
+          :listen-quotes="listenQuotes"
+
+
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
-import api from '@/services/api.js'
-
+import { onMounted, reactive, toRefs } from "vue";
+import api from "@/services/api";
+import ListQuotes from "./components/ListQuotes";
 export default {
   name: "App",
+  components: { ListQuotes},
   setup() {
     const data = reactive({
       quotes: {},
+      listenQuotes: [],
     });
-
-    api.all().then(response => {
-      data.quotes = response.data
-    })
- 
-    return {...toRefs(data)}
+    onMounted(async () => {
+      const response = await api.all();
+      data.quotes = response.data;
+    });
+  
+    return {
+      ...toRefs(data),
+    };
   },
 };
 </script>
