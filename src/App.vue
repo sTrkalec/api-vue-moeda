d13d1e055a5626704a209db141b778769780d81c
Gabriel Miranda <vytorgabriel123v@gmail.com>
2021-06-04 23:41:48 -0300

    starting a new API project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container grid-lg">
+    <input v-model="quotes.test">
+    <h1>{{quotes}}</h1>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+import api from '@/services/api.js'
+
+export default {
+  name: "App",
+  setup() {
+    const data = reactive({
+      quotes: {},
+    });
+
+    api.all().then(response => {
+      data.quotes = response.data
+    })
+ 
+    return {...toRefs(data)}
+  },
+};
+</script>
